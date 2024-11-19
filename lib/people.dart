3560869abed952d7914986cc208753119459305d
frejajo <frejfrej@outlook.com>
2024-11-19 00:49:58 -0800

    Update people.dart

--- lib/people.dart
+++ lib/people.dart
@@ -1,16 +1,22 @@
 //people page
 import 'package:flutter/material.dart';
 import 'package:arthub_demo/explore_bar.dart';
+import 'bottom_nav_bar.dart';
 
 class Explorepeoplepage extends StatelessWidget {
   const Explorepeoplepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: ExploreAppBar(),      //bottomNavigationBar: const BottomNavBar(),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          const ExploreAppBar()
+        ],
+      ),
+      bottomNavigationBar: const BottomNavBar(),
     );
   }
 }
-
 
