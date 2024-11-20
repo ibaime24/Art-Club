53a122827f7687edd47ec25e89eeee69d56b90f7
Wendi Zheng <wendizheng06@gmail.com>
2024-11-20 16:04:32 -0500

    Update interaction_design_arts.dart

--- lib/interaction_design_arts.dart
+++ lib/interaction_design_arts.dart
@@ -1,134 +1,150 @@
-//scatter art page
-import 'package:arthub_demo/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
-import 'bottom_nav_bar.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'text_styles.dart';
-import 'scatter_art.dart';
+import 'second_explore_bar.dart';
+import 'scatter_art.dart'; 
 
 class InteractionDesignArts extends StatelessWidget {
-  const InteractionDesignArts ({super.key});
-
+  const InteractionDesignArts({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Screen size for dynamic spacing
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
     
-    final idaHeight = screenHeight * 0.2;      // 20% of screen height
-    final horizontalPadding = screenWidth * 0.05; // 5% padding
-    final verticalSpacing = screenHeight * 0.02;  // 2% spacing
-    final gridSpacing = screenWidth * 0.01;       // 2% grid spacing
+    // Responsive height and spacing based on screen size
+    final idaHeight = screenHeight * 0.2; // 20% of screen height for image height
+    final horizontalPadding = screenWidth * 20 / 393; // 5% padding on both sides
+    final verticalSpacing = screenHeight * 0.02; // 2% vertical spacing for general use
+    final gridSpacing = screenWidth * 0.01; // 1% spacing between grid items
+
+    // gallery images
+    final List<String> galleryImages = [
+      'images/index9.png',
+      'images/index10.png',
+      'images/index11.png',
+      'images/index12.png',
+      'images/index13.png',
+      'images/index14.png',
+    ];
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset(
-                  'images/index6.png', 
-                  width: double.infinity,
-                  height: idaHeight,
-                  fit: BoxFit.cover,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0), // 20-pixel padding around outside border
+        child: CustomScrollView(
+          slivers: [
+            const SecondExploreBar(),
+            SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'images/index6.png',
+                    width: double.infinity,
+                    height: idaHeight,
+                    fit: BoxFit.cover,
                   ),
+                  // 15 pixels spacing between the top image and title
+                  SizedBox(height: 15),
+                  // Title: Interaction Design Arts
                   Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: horizontalPadding,
-                      vertical: verticalSpacing,),
-                      child: Text(
-                        'Interaction Deisgn Arts', 
-                        style: AppTextStyles.h1),
+                    padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+                    child: Text(
+                      'Interaction Design Arts',
+                      style: AppTextStyles.h1, 
+                    ),
                   ),
+                  // no padding between title and subtitle
                   Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: horizontalPadding),
-                      child: Text(
-                        'Practice led, experimental course taught at UALs London College of Communication',
-                        style: AppTextStyles.h3),
+                    padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+                    child: Text(
+                      'Practice led, experimental course taught at UAL’s London College of Communication',
+                      style: AppTextStyles.h3,
+                    ),
                   ),
+
+                  // Gallery and Members banner
                   Padding(
                     padding: EdgeInsets.symmetric(vertical: verticalSpacing),
                     child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text('Gallery', style: AppTextStyles.h2),
-
-                          Text('Members', style: AppTextStyles.h2.copyWith(color: Colors.grey)),
-                        ],
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('Gallery', style: AppTextStyles.h2),
+                        Text(
+                          'Members',
+                          style: AppTextStyles.h2.copyWith(color: Colors.grey),
                         ),
-                      ),
-                      
-                  ],
-                )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-          //link button start
-          SliverToBoxAdapter(
-            child: Padding(
+            ),
+            // SliverPadding for the staggered grid layout
+            SliverPadding(
               padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ScatterArtPage()),
-                  );
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  padding: EdgeInsets.symmetric(vertical: verticalSpacing),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: const Center(
-                  child: Text(
-                    'View Scatter Art',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
+              sliver: SliverMasonryGrid.count(
+                crossAxisCount: 2, 
+                mainAxisSpacing: gridSpacing,
+                crossAxisSpacing: gridSpacing,
+                childCount: galleryImages.length, // Dynamically determine the number of items (based on galleryImages)
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () {
+                    // scatter art image when clicked leads to scatter_art.dart
+                    if (index == 3) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ScatterArtPage(),
+                        ),
+                      );
+                    } else {
+                      // just open image again since links to no page
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailPage(imagePath: galleryImages[index]),
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(gridSpacing),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5), 
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5), 
+                      child: Image.asset(
+                        galleryImages[index], // Dynamic asset loading from the list
+                        fit: BoxFit.cover, 
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          //link button end
-
-          SliverPadding(
-          padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-          sliver: SliverMasonryGrid.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: gridSpacing,
-              crossAxisSpacing: gridSpacing,
-              childCount: 4,
-              itemBuilder: (context, index) => Container(
-                margin: EdgeInsets.all(gridSpacing),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 5,
-                      spreadRadius: 1,
-                    ),
-                  ],
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.asset(
-                    'images/index${index + 9}.png',  // Start from index9.png
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
+          ],
         ),
-        ]
       ),
-      bottomNavigationBar:  BottomNavBar(),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final String imagePath;
+
+  const DetailPage({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Image.asset(imagePath),
+      ),
     );
   }
 }
