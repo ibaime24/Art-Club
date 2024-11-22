4d98dc504bb6b7ae15baad3b38ece63e99b1b91b
Wendi Zheng <wendizheng06@gmail.com>
2024-11-22 03:55:45 -0500

    Update clubspage.dart

--- lib/clubspage.dart
+++ lib/clubspage.dart
@@ -1,66 +1,122 @@
-// ignore_for_file: prefer_const_constructors
-
+//clubs page
+import 'package:arthub_demo/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-//import '/text_styles.dart';
-import 'explore_art.dart';
+import 'package:arthub_demo/explore_bar.dart';
+import 'text_styles.dart';
 
 
 
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({super.key});
-  
+class Clubspage extends StatelessWidget {
+  const Clubspage({super.key});
 
-// This widget is the nav bar.
-  @override
+  //club names and descriptions
+  final List<Map<String, String>> clubs = const [
+    {'title': 'Philadelphia Arts', 'subtitle': '212 members'},
+    {'title': 'Interaction Design Arts', 'subtitle': '82 members'},
+    {'title': 'Claremont Photography Club', 'subtitle': '25 members'},
+    {'title': 'Abstract Expressionists Who Care About the Earth', 'subtitle': '19 members'},
+  ];
+      @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-          backgroundColor: Colors.white,
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.black,
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          currentIndex: 0,
-          onTap: (index) {
-            if (index == 1) {
-              Navigator.push(
-                context, 
-                MaterialPageRoute(builder: (context) => ExploreArtPage())
-                );
-            };
-            //all other buttons (indeces) do nothing
-          },
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          const ExploreAppBar(),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => ClubCard(
+                imagePath: 'images/index${index+5}.png',
+                title: clubs[index]['title']!,
+                subtitle: clubs[index]['subtitle']!,
+              ),
+              childCount: clubs.length,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.publish_outlined),
-              label: 'gallery',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.publish_outlined),
-              label: 'post',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                'assets/icons/home.svg',
-              color: Colors.black,
-              height: 40),
-              label: 'test'
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                'assets/icons/profile.svg',
-              color: Colors.black,
-              height: 24,
-              width: 24),
-              label: 'test'
-            ),
-          ]
+          ),
+        ],
+      ),
+      bottomNavigationBar: const BottomNavBar(),
     );
   }
 }
+
+class ClubCard extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String subtitle;
+
+  const ClubCard({
+    super.key,
+    required this.imagePath,
+    required this.title,
+    required this.subtitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (title == 'Interaction Design Arts')  // Check if this is the IDA card
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+              onTap: () => Navigator.pushNamed(context, '/interaction-design-arts'),
+              splashFactory: NoSplash.splashFactory,
+              highlightColor: Colors.transparent,
+              hoverColor: Colors.transparent,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Image.asset(
+                      imagePath,
+                      width: double.infinity,
+                      height: 151,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    title,
+                    style: AppTextStyles.h2,
+                  ),
+                ],
+              ),
+            ))
+          else  // For all other cards
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.asset(
+                    imagePath,
+                    width: double.infinity,
+                    height: 151,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  title,
+                  style: AppTextStyles.h2,
+                ),
+              ],
+            ),
+          const SizedBox(height: 4),
+          Text(
+            subtitle,
+            style: AppTextStyles.h3,
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+}
+
+
